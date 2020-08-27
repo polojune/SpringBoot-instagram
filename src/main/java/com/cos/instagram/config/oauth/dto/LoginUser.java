c327b5017079870e93264e52b440440c51402be1
jjk <crossfit@a.naver.com>
2020-08-27 14:44:08 +0900

    이미지 업로드 및 태그완료

--- src/main/java/com/cos/instagram/config/oauth/dto/LoginUser.java
+++ src/main/java/com/cos/instagram/config/oauth/dto/LoginUser.java
@@ -26,5 +26,10 @@ public class LoginUser {
 		this.providerId = user.getProviderId();
 	}
     
+    public User getUser() {
+    	return User.builder()
+    			.id(id)
+    			.build();
+    }
     
 }
--- src/main/java/com/cos/instagram/web/ImageController.java
+++ src/main/java/com/cos/instagram/web/ImageController.java
@@ -2,16 +2,36 @@ package com.cos.instagram.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cos.instagram.config.auth.LoginUserAnnotation;
 import com.cos.instagram.config.oauth.dto.LoginUser;
+import com.cos.instagram.service.ImageService;
+import com.cos.instagram.web.dto.ImageReqDto;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ImageController {
-
+    
+	private final ImageService imageService;
+	
 	@GetMapping({ "", "/", "/image/feed" })
 	public String feed(@LoginUserAnnotation LoginUser loginUser) {
  		System.out.println("loginUser : " + loginUser);
 		return "image/feed";
 	}
+	
+	 @GetMapping("/image/UploadForm")
+	 public String imageUploadForm() {
+		 return "image/image-upload";
+	 }
+	 
+	 @PostMapping("/image")
+	 public String imageUpload(@LoginUserAnnotation LoginUser loginUser, ImageReqDto imageReqDto) {
+		 System.out.println(imageReqDto);
+		 imageService.사진업로드(imageReqDto, loginUser.getId());
+		 return "redirect:/";
+	 }
 }

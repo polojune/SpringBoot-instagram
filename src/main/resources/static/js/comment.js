8d1e36d9e58c7991ed606a2341676c86090f609a
jjk <polo_june@naver.com>
2020-09-08 12:05:18 +0900

    회원정보 수정,프로필 사진등록 및 댓글작성

---
+++ src/main/resources/static/js/comment.js
@@ -0,0 +1,25 @@
+ function commentSend(){
+     if($("#content").val() ===""){
+    	 alert("댓글 입력이 필요합니다."); 
+         return;
+     }	
+	 
+	 
+	 
+	 let data = $("#frm").serialize(); 
+    	console.log(1,data);
+    	
+    	fetch("/comment",{
+    		method:"post", 
+    		body:data,
+    		headers:{
+    			 "Content-Type":"application/x-www-form-urlencoded; charset=utf-8"
+    		}
+    	}).then(function(res){
+    		return res.text();
+    		
+    	}).then(function(res){
+    		alert("댓글작성 성공");
+    		location.reload();
+    	});
+    }
